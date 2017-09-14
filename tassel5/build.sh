525a8b6260088af8e82bee060a82dd5d1944a817
Finlay Thompson <finlay@dragonfly.co.nz>
2017-09-14 16:49:25 +1200

    More gorbification

--- tassel5/build.sh
+++ tassel5/build.sh
@@ -1,8 +1,12 @@
-docker run -it --rm --net=host \
-    --user=$(id -u):$(id -g) \
-    -w /home/tassel5/tassel-5-standalone \
-    tassel5 \
-    ./run_pipeline.pl -configFile example_pipelines/glm_tutorial_config.xml
+#!/bin/bash
+
+set ex
+
+export IMAGE=
+export DOCKER=
+
+make all
+
+cp glm_output_tutorial* /output/
 
 
-java -classpath '$CP' $java_mem_min $java_mem_max net.maizegenetics.pipeline.TasselPipeline @args"
