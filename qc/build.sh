a6054268e0cc98829f28d9cdd037bf5c193688ea
Finlay Thompson <finlay@dragonfly.co.nz>
2017-10-25 02:35:58 +1300

    Trying a faster approach #2

--- qc/build.sh
+++ qc/build.sh
@@ -5,8 +5,8 @@ set ex
 swift --verbose list reference-data
 
 ( for f in $(swift  list reference-data | grep fastq.gz$); do
-    swift download reference-data $f -o - | zcat
-done ) | fastqc stdin
+    swift download reference-data $f -o -
+done ) | zcat | fastqc stdin
 
 ls -ltr 
  
