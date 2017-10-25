3c3421ed356d9c609146fb92695faaba54dda07f
Finlay Thompson <finlay@dragonfly.co.nz>
2017-10-25 13:06:18 +1300

    Try using curl instead of swift

--- qc/build.sh
+++ qc/build.sh
@@ -2,10 +2,13 @@
 
 set ex
 
+export token=$(swift -v stat | grep Token | sed 's/^ *Auth Token: //')
+export url=$(swift -v stat | grep Storage | sed 's/^ *StorageURL: //')
+
 swift --verbose list reference-data
 
 ( for f in $(swift  list reference-data | grep fastq.gz$); do
-    swift download reference-data $f -o -
+    curl -X GET -H "X-Auth-Token: $token" $url/reference-data/$f
 done ) | gunzip -c | fastqc -threads 4 stdin
 
 ls -ltr 
