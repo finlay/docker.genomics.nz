a4eefe4e3d730aa5617f95097259f7faf74c8a2b
Finlay Thompson <finlay@dragonfly.co.nz>
2017-10-30 09:11:44 +1300

    Updated to save file locally before running fastqc

--- qc/build.sh
+++ qc/build.sh
@@ -7,9 +7,14 @@ export url=$(swift -v stat | grep Storage | sed 's/^ *StorageURL: //')
 
 swift --verbose list reference-data
 
-( for f in $(swift  list reference-data | grep fastq.gz$); do
-    curl -X GET -H "X-Auth-Token: $token" $url/reference-data/$f
-done ) | gunzip -c | fastqc -threads 4 stdin
+swift list reference-data | \
+    grep fastq.gz$ | \
+    sed "s@^@$url/reference-data/@" | \
+    xargs -n 1  \
+    curl -X GET -H "X-Auth-Token: $token"  \
+    > all.fastq.gz
+
+fastqc -threads 4 all.fastq.gz
 
 ls -ltr 
  
